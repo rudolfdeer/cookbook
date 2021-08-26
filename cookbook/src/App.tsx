760326eb460b7c92dee29eb7e920c191becf2c0a
Toma Lichko <tomalichko@gmail.com>
2021-08-26 16:38:40 +0300

    init: set up environment

---
+++ cookbook/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(): JSX.Element {
+  return (
+    <>
+      <header className="header">Header</header>
+      <main className="main">Main</main>
+    </>
+  );
+}
+
+export default App;
---
+++ cookbook/src/react-app-env.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react-scripts" />
+TSC_COMPILE_ON_ERROR = true;
+ESLINT_NO_DEV_ERRORS = true;

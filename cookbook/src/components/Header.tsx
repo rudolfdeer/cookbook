370ce23a5b9c7db8c4d9401402b89eff76092782
Toma Lichko <tomalichko@gmail.com>
2021-08-27 17:00:37 +0300

    fix: fix styles

--- cookbook/src/components/Header.tsx
+++ cookbook/src/components/Header.tsx
@@ -7,15 +7,15 @@ export default function Header(): JSX.Element {
   return (
     <header className="header">
       <Link to="/"><div className="logo-small" /></Link>
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-list-item"><Link to="/recepies">Recepies</Link></li>
-          <li className="nav-list-item"><Link to="/cookbooks">Cookbooks</Link></li>
+      <nav>
+        <ul className="header-nav-list">
+          <li className="header-nav-list-item"><Link to="/recepies">Recepies</Link></li>
+          <li className="header-nav-list-item"><Link to="/cookbooks">Cookbooks</Link></li>
         </ul>
       </nav>
-      <div className="header-search-container">
-        <div className="search-icon-small" />
-        <input type="text" className="header-input-search" />
+      <div className="header-search">
+        <div className="header-search-icon" />
+        <input type="text" className="header-search-input" />
       </div>
       <button className="header-btn-create btn light"><Link to="/create_cookbook">Create CookBook</Link></button>
       <a href="#!" className="header-link-login"><Link to="/login">Sign in</Link></a>
--- cookbook/src/pages/home-page.tsx
+++ cookbook/src/pages/home-page.tsx
@@ -6,24 +6,24 @@ export default function HomePage(): JSX.Element {
     <>
       <div className="wrapper">
         <Header />
-        <div className="intro">
-          <div className="intro-content">
-          <h1 className="intro-title">Find Recipies and Сreate Your Favourite Сookbooks</h1>
-          <div className="search-container">
-            <div className="search-icon" />
-            <input type="text" className="intro-input-search" placeholder="Find Best Recipies..." />
-            <button className="intro-btn-search btn dark">Search</button>
-          </div>
-          <nav className="intro-nav">
-            <ul className="intro-nav-list">
-              <li className="nav-list-item">Vegeterian</li>
-              <li className="nav-list-item">Mexican</li>
-              <li className="nav-list-item">Greece Kithcen</li>
-              <li className="nav-list-item">Italy Pizza</li>
-              <li className="nav-list-item">Philippines</li>
-              <li className="nav-list-item">Japan Sushi</li>
-            </ul>
-          </nav>
+        <div className="intro-container">
+          <div className="intro">
+            <h1 className="intro-title">Find Recipies and Сreate Your Favourite Сookbooks</h1>
+            <div className="intro-search">
+              <div className="intro-search-icon" />
+              <input type="text" className="intro-search-input" placeholder="Find Best Recipies..." />
+              <button className="intro-search-btn btn dark">Search</button>
+            </div>
+            <nav>
+              <ul className="intro-nav-list">
+                <li className="intro-nav-list-item">Vegeterian</li>
+                <li className="intro-nav-list-item">Mexican</li>
+                <li className="intro-nav-list-item">Greece Kithcen</li>
+                <li className="intro-nav-list-item">Italy Pizza</li>
+                <li className="intro-nav-list-item">Philippines</li>
+                <li className="intro-nav-list-item">Japan Sushi</li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>

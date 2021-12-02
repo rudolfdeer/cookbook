c80e3f7891ec95f6a19db351b7689465f672e4f8
Toma Lichko <tomalichko@gmail.com>
2021-12-02 16:41:13 +0300

    feat: add change language function

--- cookbook/client/src/components/Footer/index.tsx
+++ cookbook/client/src/components/Footer/index.tsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { COOKBOOKS, RECIPES } from '../../constants/resources/common';
-import {
-  ABOUT_US,
-  EMAIL,
-  ITECHART,
-  ITECHART_SITE,
-  PROJECT,
-} from '../../constants/resources/footer';
+import { useTranslation } from 'react-i18next';
+// import { COOKBOOKS, RECIPES } from '../../constants/resources/common';
+// import {
+//   ABOUT_US,
+//   EMAIL,
+//   ITECHART,
+//   ITECHART_SITE,
+//   PROJECT,
+// } from '../../constants/resources/footer';
 import ROUTES from '../../constants/routes';
 
 import './index.scss';
 
 export default function Footer(): JSX.Element {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (e: React.MouseEvent) => {
+    const target = e.target as HTMLButtonElement;
+    i18n.changeLanguage(target.value);
+  };
+
   return (
     <footer className="footer">
       <div className="wrapper">
@@ -23,24 +31,40 @@ export default function Footer(): JSX.Element {
           <nav className="footer__nav">
             <ul className="footer__nav__list">
               <li className="footer__nav__list__item">
-                <Link to={ROUTES.COOKBOOKS}>{COOKBOOKS}</Link>
+                <Link to={ROUTES.COOKBOOKS}>{t('COOKBOOKS')}</Link>
               </li>
               <li className="footer__nav__list__item">
-                <Link to={ROUTES.RECIPES}>{RECIPES}</Link>
+                <Link to={ROUTES.RECIPES}>{t('RECIPES')}</Link>
               </li>
               <li className="footer__nav__list__item">
-                <Link to="/">{ABOUT_US}</Link>
+                <Link to="/">{t('ABOUT_US')}</Link>
               </li>
             </ul>
           </nav>
           <div className="footer__email">
-            <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
+            <a href={`mailto:${t('MY_EMAIL')}`}>{t('MY_EMAIL')}</a>
           </div>
           <div className="footer__study">
-            <div className="footer__study__project">{PROJECT}</div>
+            <div className="footer__study__project">{t('PROJECT')}</div>
             <div className="footer__study__itechart">
-              <a href={ITECHART_SITE}>{ITECHART}</a>
+              <a href={t('ITECHART_SITE')}>{t('ITECHART')}</a>
             </div>
+          </div>
+          <div className="footer__lang">
+            <button
+              className="footer__lang__btn"
+              value="en"
+              onClick={(e) => changeLanguage(e)}
+            >
+              English
+            </button>
+            <button
+              className="footer__lang__btn"
+              value="fr"
+              onClick={(e) => changeLanguage(e)}
+            >
+              Français
+            </button>
           </div>
         </div>
       </div>
